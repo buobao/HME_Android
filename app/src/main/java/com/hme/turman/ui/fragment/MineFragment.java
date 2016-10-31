2edcf58b2198b8a364bdef2e7330eae889487306
buobao <320683buobao>
2016-10-31 23:39:12 +0800

    formitemlayout update

--- app/src/main/java/com/hme/turman/ui/fragment/MineFragment.java
+++ app/src/main/java/com/hme/turman/ui/fragment/MineFragment.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 
 import com.hme.turman.R;
 import com.hme.turman.base.BaseFragment;
+import com.hme.turman.widgets.FormItemLayout;
+
+import butterknife.BindView;
 
 /**
  * Created by diaoqf on 2016/10/31.
  */
 
 public class MineFragment extends BaseFragment {
+    @BindView(R.id.my_message)
+    FormItemLayout my_message;
+
     @Override
     protected void init(Bundle savedInstanceState) {
-
+        my_message.setOnItemClickListener(v->{
+            toast("我的动态");
+        });
     }
 
     @Override
--- app/src/main/java/com/hme/turman/widgets/FormItemLayout.java
+++ app/src/main/java/com/hme/turman/widgets/FormItemLayout.java
@@ -1,21 +1,68 @@
 package com.hme.turman.widgets;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hme.turman.R;
 
 /**
  * Created by diaoqf on 2016/10/31.
  */
 
 public class FormItemLayout extends LinearLayout {
+    private ImageView left_image;
+    private TextView title;
+    private LinearLayout click_layout;
+    private TextView hint;
+
+    private OnItemClickListener onItemClickListener;
+
+
     public FormItemLayout(Context context) {
         super(context);
+
+        init(context,null);
     }
 
     public FormItemLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
+    private void init(Context context, AttributeSet attrs) {
+        View view = LayoutInflater.from(context).inflate(R.layout.wgt_form_item_layout, this, true);
+        left_image = (ImageView) findViewById(R.id.left_image);
+        title = (TextView) findViewById(R.id.title);
+        click_layout = (LinearLayout) findViewById(R.id.click_layout);
+        hint = (TextView) findViewById(R.id.hint);
+
+        click_layout.setOnClickListener((v)->{
+            if (onItemClickListener != null) {
+                onItemClickListener.onClick(view);
+            }
+        });
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FormItemLayout);
+
+        left_image.setImageDrawable(getResources().getDrawable(a.getResourceId(R.styleable.FormItemLayout_left_img, R.drawable.ic_main_tab_one_nor)));
+        title.setText(a.getText(R.styleable.FormItemLayout_title));
+        hint.setText(a.getText(R.styleable.FormItemLayout_hint));
+        a.recycle();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener{
+        void onClick(View v);
+    }
 
 }
