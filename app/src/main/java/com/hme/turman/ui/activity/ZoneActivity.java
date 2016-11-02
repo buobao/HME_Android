2659eb25c2c5634f264e1a386ae95c758db371f7
diaoqf <diaoqf@centaline.com.cn>
2016-11-02 17:36:06 +0800

    1102 zone activity

--- app/src/main/java/com/hme/turman/ui/activity/ZoneActivity.java
+++ app/src/main/java/com/hme/turman/ui/activity/ZoneActivity.java
@@ -5,10 +5,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hme.turman.CacheData;
 import com.hme.turman.R;
 import com.hme.turman.base.BaseActivity;
+import com.hme.turman.widgets.SimpleItemLayout;
 
 import butterknife.BindView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by diaoqf on 2016/11/2.
@@ -23,6 +26,29 @@ public class ZoneActivity extends BaseActivity {
     @BindView(R.id.back)
     ImageView back;
 
+    @BindView(R.id.head_img)
+    CircleImageView head_img;
+    @BindView(R.id.user_name)
+    TextView user_name;
+    @BindView(R.id.user_location)
+    TextView user_location;
+
+    @BindView(R.id.user_home_address)
+    SimpleItemLayout user_home_address;
+    @BindView(R.id.user_active_address)
+    SimpleItemLayout user_active_address;
+    @BindView(R.id.user_age)
+    SimpleItemLayout user_age;
+    @BindView(R.id.user_company)
+    SimpleItemLayout user_company;
+    @BindView(R.id.user_affective_state)
+    SimpleItemLayout user_affective_state;
+    @BindView(R.id.user_sign)
+    SimpleItemLayout user_sign;
+    @BindView(R.id.user_hobby)
+    SimpleItemLayout user_hobby;
+
+
     @Override
     protected void init(Bundle savedInstanceState) {
         title.setText("我的主页");
@@ -34,6 +60,13 @@ public class ZoneActivity extends BaseActivity {
         title_menu.setOnClickListener(v->{
             toast("到编辑页面");
         });
+
+        user_name.setText(CacheData.getDefault().getUserName());
+        String address = CacheData.getDefault().getUserAddress();
+        if (address != null) {
+            user_location.setText(address);
+        }
+
     }
 
     @Override
