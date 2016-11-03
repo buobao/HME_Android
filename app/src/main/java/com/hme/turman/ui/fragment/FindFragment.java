5ff2e7df27e22cb9cf865e622c5dc4c0f915d7a6
diaoqf <diaoqf@centaline.com.cn>
2016-11-03 18:58:22 +0800

    1103 event item

--- app/src/main/java/com/hme/turman/ui/fragment/FindFragment.java
+++ app/src/main/java/com/hme/turman/ui/fragment/FindFragment.java
@@ -1,7 +1,11 @@
 package com.hme.turman.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hme.turman.R;
@@ -23,6 +27,11 @@ public class FindFragment extends BaseFragment {
     @BindView(R.id.title_menu)
     TextView title_menu;
 
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipe_refresh_layout;
+    @BindView(R.id.listview)
+    ListView listView;
+
     private int currentType;
 
     @Override
@@ -31,6 +40,30 @@ public class FindFragment extends BaseFragment {
         title_menu.setVisibility(View.VISIBLE);
         title_menu.setText("地图");
         currentType = LIST_VIEW;
+
+        swipe_refresh_layout.setOnRefreshListener(()->{
+
+        });
+
+        swipe_refresh_layout.setColorSchemeResources(
+                R.color.app_color, R.color.app_color_1,
+                R.color.app_color_2, R.color.app_color_3);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+        });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
     }
 
     @Override
