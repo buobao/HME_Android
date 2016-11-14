721b05d273700e7efebace0053d8281b97fc439f
diaoqf <diaoqf@centaline.com.cn>
2016-11-14 18:45:41 +0800

    11.14 create userinfo edit activity

---
+++ app/src/main/java/com/hme/turman/ui/activity/UserInfoEditActivity.java
@@ -0,0 +1,22 @@
+package com.hme.turman.ui.activity;
+
+import android.os.Bundle;
+
+import com.hme.turman.R;
+import com.hme.turman.base.BaseActivity;
+
+/**
+ * Created by diaoqf on 2016/11/14.
+ */
+
+public class UserInfoEditActivity extends BaseActivity {
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getContentLayout() {
+        return R.layout.act_user_info_edit;
+    }
+}
