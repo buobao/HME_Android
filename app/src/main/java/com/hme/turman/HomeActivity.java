1c33de7a43fa3e0d0a98bc905ff551104e4fb297
diaoqf <diaoqf@centaline.com.cn>
2016-11-28 08:46:49 +0800

    11.28 update

--- app/src/main/java/com/hme/turman/HomeActivity.java
+++ app/src/main/java/com/hme/turman/HomeActivity.java
@@ -19,6 +19,8 @@ import butterknife.ButterKnife;
 import cn.jpush.android.api.JPushInterface;
 import cn.jpush.android.api.TagAliasCallback;
 import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
@@ -36,30 +38,30 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_home);
-        ButterKnife.bind(this);
-        EventBus.getDefault().register(this);
-        test.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MapActivity.class);
-            startActivity(intent);
-        });
+//        ButterKnife.bind(this);
+//        EventBus.getDefault().register(this);
+//        test.setOnClickListener(v -> {
+//            Intent intent = new Intent(this, MapActivity.class);
+//            startActivity(intent);
+//        });
 
-        Observable.just("Hello world")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        Toast.makeText(HomeActivity.this,s,Toast.LENGTH_SHORT).show();
-                    }
-                });
+//        Observable.just("Hello world")
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Action1<String>() {
+//                    @Override
+//                    public void call(String s) {
+//                        Toast.makeText(HomeActivity.this,s,Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 
         //通过设置别名推送
-        JPushInterface.setAlias(getApplicationContext(), "buobao", new TagAliasCallback() {
-            @Override
-            public void gotResult(int i, String s, Set<String> set) {
-                Logger.i("别名设置回调:"+i);
-            }
-        });
+//        JPushInterface.setAlias(getApplicationContext(), "buobao", new TagAliasCallback() {
+//            @Override
+//            public void gotResult(int i, String s, Set<String> set) {
+//                Logger.i("别名设置回调:"+i);
+//            }
+//        });
 
 //        addTask();
 //        ApiHelper.getTestText().subscribeOn(Schedulers.io())
@@ -70,12 +72,39 @@ public class HomeActivity extends Activity {
 //                        getfromserver.setText(s);
 //                    }
 //                });
+
+        Observable<String> stringObservable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                for (int i=0;i<10;i++) {
+                    subscriber.onNext("Hello RxJava");
+                }
+                subscriber.onCompleted();
+            }
+        });
+
+        Subscription subscription = stringObservable.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Logger.i("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Logger.i("onError");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Logger.i(s);
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        EventBus.getDefault().unregister(this);
+//        EventBus.getDefault().unregister(this);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
